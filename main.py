03fa1b92b409c8db369afe110f5e3f89f2349e4d
buboribencebarnabas <buboribencebarnabas@lechnerkozpont.hu>
2020-12-03 20:54:55 +0100

    bugfix

--- main.py
+++ main.py
@@ -1,13 +1,15 @@
 import repository.accomodation as service
 import repository.csv_parser as csv
 from service import osm_scraper
+import pandas as pd
 
 townData = csv.parse_package('./teir_adatok_2016/telepules/')
 countyData = csv.parse_package('./teir_adatok_2016/megye/')
 
 townCoordinates = osm_scraper.scrape_page(countyData[0]['Megnevezés'])
 
-
+pd.DataFrame.from_dict(townCoordinates)\
+    .to_csv('countyCoordinates.csv')
 print(townCoordinates)
 
 #print(service.loadTable())
--- repository/csv_parser.py
+++ repository/csv_parser.py
@@ -3,11 +3,8 @@ import numpy as np
 from os import listdir
 from os.path import isfile, join
 
-from model.csv_dataframe import CsvDataFrame
-
 
 def parse_package(path):
-
     all_files = [f for f in listdir(path) if isfile(join(path, f))]
     csv_files = list(filter(lambda x: x.endswith('.csv'), all_files))
 
@@ -17,17 +14,20 @@ def parse_package(path):
         dataframe = pd.read_csv(path + filename)
 
         column_indexes = np.where(pd.isnull(dataframe['Megnevezés']))[0]
-
         column_names = dataframe.iloc[column_indexes[0] + 1:column_indexes[1]]
-
-
-        for column_name in column_names['Megnevezés'].items():
-            title = column_names['Kód'][column_name[0]]
-            dataframe.rename(columns={column_name[1]: title}, inplace=True)
-
-        csv_dataframe = CsvDataFrame(dataframe, column_names[['Kód', 'Megnevezés']])
+        map_column_names(dataframe, column_names)
 
         count_of_meta_lines = column_indexes[1] - column_indexes[0]
 
-        package.append(dataframe.iloc[: - count_of_meta_lines - 1])
+        dataframe = dataframe.iloc[: - count_of_meta_lines - 1]
+        dataframe.name = filename
+        package.append(dataframe)
+
     return package
+
+
+def map_column_names(dataframe, column_names):
+    for column_name in column_names['Megnevezés'].items():
+        title = column_names['Kód'][column_name[0]]
+        dataframe.rename(columns={column_name[1]: title}, inplace=True)
+        dataframe.rename(columns={f'SUM({column_name[1]})': title}, inplace=True)
