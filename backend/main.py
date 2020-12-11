d38ef195e6d362edbedb4aa8ee9ad56ef3032b61
buboribencebarnabas <buboribencebarnabas@lechnerkozpont.hu>
2020-12-11 13:05:27 +0100

    little refactor

--- backend/main.py
+++ backend/main.py
@@ -1,13 +1,14 @@
 from flask import Flask, jsonify
 from flask_cors import CORS
-import service.csv_parser as csv
+import service.csv_parser as parser
 import service.coordinate_service as service
 from service.options import county_path, town_path
+import pandas as pd
 
 print(town_path)
 
-townData = csv.parse_package(town_path)
-countyData = csv.parse_package(county_path)
+townData = parser.parse_package(town_path)
+countyData = parser.parse_package(county_path)
 
 app = Flask(__name__)
 CORS(app)
@@ -23,15 +24,23 @@ def hello_world():
 @app.route('/town')
 def town_csv():
     return jsonify({
-        'telepules': csv.get_all_csv(town_path)
+        'town': parser.get_all_csv(town_path)
     })
 
 
 @app.route('/county')
 def county_csv():
     return jsonify({
-        'megye': csv.get_all_csv(county_path)
+        'county': parser.get_all_csv(county_path)
     })
+
+
+@app.route('/<package>/<csv>')
+def page(package, csv):
+    dataframe = parser.parse_csv('./teir_adatok_2016/' + package + '/' + csv)  # type: pd.Dataframe
+    dataframe.name.rename(columns={'id': 'title', 'title': 'id'}, inplace=True)
+
+    return dataframe.name.to_json()
 
 
 if __name__ == '__main__':
--- backend/service/csv_parser.py
+++ backend/service/csv_parser.py
@@ -10,20 +10,25 @@ def parse_package(path):
     package = []
 
     for filename in csv_files:
-        dataframe = pd.read_csv(path + filename)
-        fix_column_names(dataframe)
-
-        column_indexes = np.where(pd.isnull(dataframe['title']))[0]
-        column_names = dataframe.iloc[column_indexes[0] + 1:column_indexes[1]]
-        # map_column_names(dataframe, column_names)
-
-        count_of_meta_lines = column_indexes[1] - column_indexes[0]
-
-        dataframe = dataframe.iloc[: - count_of_meta_lines - 1]
-        dataframe.name = column_names[['id', 'title']]
-        package.append(dataframe)
+        package.append(parse_csv(path + filename))
 
     return package
+
+
+def parse_csv(path):
+    dataframe = pd.read_csv(path)
+    fix_column_names(dataframe)
+
+    column_indexes = np.where(pd.isnull(dataframe['title']))[0]
+    column_names = dataframe.iloc[column_indexes[0] + 1:column_indexes[1]]
+    # map_column_names(dataframe, column_names)
+
+    count_of_meta_lines = column_indexes[1] - column_indexes[0]
+
+    dataframe = dataframe.iloc[: - count_of_meta_lines - 1]
+    dataframe.name = column_names[['id', 'title']]
+
+    return dataframe
 
 
 def get_all_csv(path):
