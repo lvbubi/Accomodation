f7717c955d96950e1289f2f3e2fe658668cc0a38
buboribencebarnabas <buboribencebarnabas@lechnerkozpont.hu>
2020-12-14 15:46:52 +0100

    fix reference error

--- backend/controller/layer_controller.py
+++ backend/controller/layer_controller.py
@@ -5,9 +5,11 @@ from main import app, full_dictionary
 @app.route('/percentage/<root>/<csv>/<column>')
 def column_percentage(root, csv, column):
     df = full_dictionary[root][csv]
-    df[column] = df[column].transform(lambda x: x / x.sum())
 
-    subset = df[['id', 'coordinate', column]]
+    df_probability = df[column].transform(lambda x: x / x.sum())
+
+    subset = df[['id', 'coordinate']]
+    subset[column] = df_probability
 
     return jsonify([tuple(x) for x in subset.to_numpy()])
 
